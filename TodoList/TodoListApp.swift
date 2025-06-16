29560a260fa741e8aca05b9c03073d7c307a4aa8
Milos Taskovic <milos.taskovic@adiglobal.com>
2025-06-16 09:52:58 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Milos Taskovic on 16. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
